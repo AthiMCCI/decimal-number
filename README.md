18c8b4939fe892c3c19703210319c97e7a5d2462
“Athisankar” <athisankar@mcci.com>
2022-05-23 14:28:44 +0530

    fix sources with coding

--- README.md
+++ README.md
@@ -1,8 +1,7 @@
-
 # count zeros and ones in a binary number:
 To create a program which is used to find the number of binary ones and binary zeroes in a given decimal numbers.
 # Working:
-- In this program,we are reading the decimal number using  variable.
+- in  this program,we are reading the decimal number using  variable.
 - A decimal number system is a base number system using digits.
 - Check whether the number is less than or equal to zero.
 - Divide the number and store the remainder.
--- ones-and-zeros-in-a-given-decimal-number.c
+++ ones-and-zeros-in-a-given-decimal-number.c
@@ -1,17 +1,26 @@
 /*
-Module: ones-and-zeros-in-a-given-decimal-number.c
+
+Module:
+    ones_and_zeros_in_a_given_decimal_number.c
+
 Function:
-    Finding Binary zeros and Ones in a given decimal  number.
+    Finding Binary zeros and Ones in a given decimal number.
+
 Copyright notice:
     This file copyright (C) 2022 by
+
     MCCI Corporation
     3520 Krums Corners Road
     Ithaca, NY 14850
+
     An unpublished work. All rights reserved.
+
     This file is proprietary information, and may not be disclosed or
     copied without the prior permission of MCCI Corporation.
+
 Author:
     Athisankar, MCCI Corporation   May 2022
+
 */
 
 #include<stdio.h>
@@ -23,19 +32,29 @@ Author:
 \****************************************************************************/
 
 /*
+
 Name: main()
+
 Function:
     To compute Binary zeros and Ones in a given decimal number.
- and to diplay the output.
+    and to diplay the output.
+
 Definition:
-    Void Function();
+    int main(
+    void
+    );
+
 Returns:
     Functions returning type: integer.
+
 */
 
-int main()
+int main(void)
     {
-    int ch;
+    int number;
+    int countOne;
+    int countZero;
+    int bit;
 
     printf("\n");
     printf("*******************************************************\n");
@@ -45,54 +64,50 @@ int main()
     printf("It is used to find the number of binary zeroes and ones\n");
     printf("in a given decimal number.\n\n");
 
-    int number, countone, countzero, bit;
-    countone = 0;
-    countzero = 0;
 
-    // Getting decimal input and printing it's Binary output.
+    countOne = 0;
+    countZero = 0;
 
+    /* Getting decimal input and printing it's Binary output. */
     printf("\nEnter a Decimal Number: ");
-    scanf("%d", & number);
+    scanf("%d", &number);
 
     printf("\nThe Binary value of your number is: ");
 
-    // Loop condition to print Binary Value.
-
+    /* Loop condition to print Binary Value. */
     for (bit = 31; bit >= 0; --bit)
         {
-        int bin = number & (1 << bit);
+        int bin;
+
+        bin = number & (1 << bit);
 
         if (bin == 0)
-            printf("0");
+        printf("0");
         else
-            printf("1");
+        printf("1");
 
         if(bit % 4 == 0 && bit != 0)
-            printf("|");
+        printf("|");
         }
 
     printf("\n");
 
-    // While the given number is greater than zero, do the condition
-
+    /* While the given number is greater than zero, do the condition */
     while (number > 0)
         {
-        //comparing binary value of num with 1 using bitwise operator
-
+        /* comparing binary value of num with 1 using bitwise operator */
         if (number & 1 == 1)
-            countone++;                                 // if true, increment one
+        countOne++;                                 /* if true, increment one */
         else
-            countzero++;                                // if false increment zero
+        countZero++;                                /* if false increment zero */
 
-        // rightshifting the binary number by 1 digit
-
+        /* rightshifting the binary number by 1 digit */
         number = number >> 1;
         }
 
-    // Printing number of ones and zeros
-
-    printf("\nThe number of Ones is: %d\n", countone);
-    printf("The number of zeros is: %d\n", countzero);
+    /* Printing number of ones and zeros */
+    printf("\nThe number of Ones is: %d\n", countOne);
+    printf("The number of zeros is: %d\n", countZero);
 
     return 0;
     }
